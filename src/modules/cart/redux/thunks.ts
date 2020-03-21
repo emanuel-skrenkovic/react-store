b5d013fc501e90d896ab412aaab0d43a706618d8
Emanuel Skrenković <eskrenkovic@gmail.com>
2020-03-21 18:02:16 +0100

    cart items now stored in storage for logged-in users

--- src/modules/cart/redux/thunks.ts
+++ src/modules/cart/redux/thunks.ts
@@ -2,16 +2,12 @@ import { ThunkDispatch } from 'redux-thunk';
 
 import { ApplicationState, ShopItem } from 'models';
 import { selectAuthInfo } from 'modules/authentication';
-import { addItemToCart } from 'modules/common';
+import { addItemToCart, removeItemFromCart } from 'modules/common';
 import { CartAction, addItem, removeItem } from 'modules/cart';
 
 export const attemptAddItem = (item: ShopItem) =>
     async (dispatch: ThunkDispatch<ApplicationState, void, CartAction>, getState: () => ApplicationState) => {
-        console.log('reached attempAddItem');
         const { isSignedIn, user } = selectAuthInfo(getState());
-
-        console.log(isSignedIn);
-        console.log(user);
 
         if (isSignedIn && user) {
             await addItemToCart(user.userId, item);
@@ -21,13 +17,13 @@ export const attemptAddItem = (item: ShopItem) =>
 };
 
 export const attemptRemoveItem = (id: string) =>
-    async (dispatch: ThunkDispatch<ApplicationState, void, CartAction>) => {
+    async (dispatch: ThunkDispatch<ApplicationState, void, CartAction>, getState: () => ApplicationState) => {
 
-        // const { isSignedIn } = useSelector(selectAuthInfo);
-        //
-        // if (isSignedIn) {
-        //     // TODO update firebase
-        // }
+        const { isSignedIn, user } = selectAuthInfo(getState());
+
+        if (isSignedIn && user) {
+            await removeItemFromCart(user.userId, id);
+        }
 
         dispatch(removeItem(id));
 };
--- src/modules/common/providers/cart.ts
+++ src/modules/common/providers/cart.ts
@@ -1,27 +1,39 @@
+import * as firebase from 'firebase';
 import 'firebase/firestore';
 
-import { ShopItem } from 'models';
+import { CartItem, ShopItem } from 'models';
 import { store }  from "modules/common";
 
+const CARTS = 'carts';
+const ITEMS = 'items';
+
 export const addItemToCart = async (userId: string, item: ShopItem): Promise<void> => {
-    await store.collection('carts')
+    await store.collection(CARTS)
         .doc(userId)
-        .collection('items')
+        .collection(ITEMS)
         .doc(item.id)
-        .set(item);
+        .set({
+            item: item,
+            count: firebase.firestore.FieldValue.increment(1)
+        }, { merge: true });
 };
 
 export const removeItemFromCart = async (userId: string, itemId: string): Promise<void> => {
-    // const itemDocumentData: firebase.firestore.DocumentData = await store.collection('carts')
-    //     .doc(userId)
-    //     .collection('items')
-    //     .doc(itemId)
-    //     .get();
-    //
-    // const item: ShopItem = itemDocumentData.data() as ShopItem;
-    //
-    // const cartDocumentData: firebase.firestore.DocumentData = await store.collection('carts')
-    //     .doc(userId)
-    //     .collection('items')
-    //     .where('id', '==', itemId)
+    const itemDocumentData: firebase.firestore.DocumentData = await store.collection(CARTS)
+        .doc(userId)
+        .collection(ITEMS)
+        .doc(itemId)
+        .get();
+
+    const item: CartItem = itemDocumentData.data() as CartItem;
+
+    if (item.count > 1) {
+        await store.collection(CARTS)
+            .doc(userId)
+            .collection(ITEMS)
+            .doc(itemId)
+            .update('count', firebase.firestore.FieldValue.increment(-1));
+    } else {
+        await store.collection(CARTS).doc(userId).collection(ITEMS).doc(itemId).delete();
+    }
 };
