4717aa30d54469d4f04e2e50761a782c725fa8ed
Emanuel Skrenković <eskrenkovic@gmail.com>
2020-03-22 13:06:13 +0100

    fixed firebase imports

--- src/modules/common/providers/authentication.ts
+++ src/modules/common/providers/authentication.ts
@@ -1,4 +1,4 @@
-import * as firebase from "firebase";
+import * as firebase from "firebase/app";
 
 import { ApplicationUser, UserRole } from "models";
 import { auth, store, googleAuth } from 'modules/common/providers';
