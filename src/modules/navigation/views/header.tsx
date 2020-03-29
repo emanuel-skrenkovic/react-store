3adc52d434ab0c62b85fd10ed2a540b6208cb9c1
Emanuel Skrenković <eskrenkovic@gmail.com>
2020-03-29 23:25:39 +0200

    changed header menu to tabular

--- src/modules/navigation/views/header.tsx
+++ src/modules/navigation/views/header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import { selectAuthInfo, attemptSignOut } from 'modules/authentication';
 import { selectCartItemCount } from 'modules/cart';
@@ -28,19 +28,25 @@ export const Header: React.FC = () => {
         );
     };
 
+    const { pathname } = useLocation();
+    const getTabStyle = (linkPath: string) => {
+        return linkPath === pathname
+            ? 'active item'
+            : 'item';
+    };
+
     return (
-        <div className="ui secondary pointing menu">
+        <div className="ui tabular menu">
             <span>Hi!{' '}{renderAuth()}</span>
-            <Link className="item" to="/home">Home</Link>
-            <Link className="item" to="/listing">Listing</Link>
-            <Link className="item" to="/faq">FAQ</Link>
-            {user && user.role === UserRole.Admin && <Link to="/admin" className="item">Administration</Link>}
-            <div className="right menu">
+            <Link className={getTabStyle('/home')} to="/home">Home</Link>
+            <Link className={getTabStyle('/listing')} to="/listing">Listing</Link>
+            <Link className={getTabStyle('/faq')} to="/faq">FAQ</Link>
+            {user && user.role === UserRole.Admin && <Link to="/admin" className={getTabStyle('/admin')}>Administration</Link>}
+            <div className="ui tabular right menu">
                 <div>
-                    <Link to="/cart" className="item">Cart</Link>
+                    <Link to="/cart" className={getTabStyle('/cart')}>Cart</Link>
                     {cartItemCount > 0 &&
-                        <div className="floating ui teal label">{cartItemCount}</div>
-                    }
+                        <div className="floating ui teal label">{cartItemCount}</div>}
                 </div>
 
                 {isSignedIn
