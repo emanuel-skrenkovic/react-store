63e42b12529368daa0877cd853cc14ebefa58657
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2020-05-01 15:36:38 +0200

    rewrote isPromise to use Typescript features

--- src/modules/common/helpers.ts
+++ src/modules/common/helpers.ts
@@ -1,8 +1,8 @@
 import { Dictionary, IndexedEntity } from 'models';
 
-export const isPromise = (obj: any): boolean => {
-    return !!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function';
-};
+export const isPromise = (obj: any): obj is Promise<any> => {
+    return obj && (obj as Promise<any>).then !== undefined;
+}
 
 export const convertArrayToMap = <T extends IndexedEntity>(arr: T[]): Dictionary<string, T> => {
     return arr.reduce((acc: Dictionary<string, T>, x: T) => {
